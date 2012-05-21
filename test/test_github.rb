29d4ba3a45f07172d2cfd5d30feb4d2c2f1946a0
Tristan O'Neil <tristanoneil@gmail.com>
2012-05-21 12:06:31 -0400

    Update test.

--- test/test_github.rb
+++ test/test_github.rb
@@ -19,7 +19,7 @@ class TestGithub < Test::Unit::TestCase
   end
 
   def test_stream
-    assert_equal "Tristan O'Neil pushed to <a href='http://github.com/tristanoneil/tristanoneil.github.com'>tristanoneil/tristanoneil.github.com</a>.", @github.stream[0][:message]
+    assert_equal "Tristan O'Neil pushed <a href='http://github.com/tristanoneil/tristanoneil.github.com/commit/c1e38d24268245b9336e2734e6864c7266c79284'>c1e38d24268245b9336e2734e6864c7266c79284</a> to <a href='http://github.com/tristanoneil/tristanoneil.github.com'>tristanoneil/tristanoneil.github.com</a>.", @github.stream[0][:message]
     assert_equal "2012-05-11 20:17:30 UTC", @github.stream[0][:created_at].to_s
     assert_equal 30, @github.stream.size
   end
